f0349f845e38b6b87e4a381662f2c4e6c9b88588
KimMinSeo-util <53888169+KimMinSeo-util@users.noreply.github.com>
2020-06-04 13:10:06 +0900

    Create StringApp.java
    Day03

---
+++ StringApp.java
@@ -0,0 +1,21 @@
+
+public class StringApp {
+	public static void main(String[] args) {
+		
+		// Character VS String
+		System.out.println("Hello World"); // String
+		System.out.println('H'); // Character(한 글자)
+		System.out.println("H");
+		
+		System.out.println("Hello "
+				+ "World");
+		
+		// \n -> new line
+		System.out.println("Hello \nWorld");
+		
+		// escape -> \"World\"
+		System.out.println("Hello \"World\"");// Hello "World"
+
+	}
+
+}
